722a1edf63837253589531479cf7a62ffb0b3f96
Conner Pinson <conner.pinson@hey.com>
2020-07-05 13:33:09 -0500

    change socials, verify mstdn

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import { FaLinkedin, FaGithub, FaTwitter, FaYoutube, FaEnvelope } from 'react-icons/fa'
+import { FaLinkedin, FaGithub, FaTwitter, FaYoutube, FaEnvelope, FaMastodon, FaKeybase } from 'react-icons/fa'
 
 const Header = ({ siteTitle }) => (
   <header
@@ -70,7 +70,8 @@ const Header = ({ siteTitle }) => (
       <div style={{paddingTop: `1.0875rem`}}>
         <a style={{color: 'white', textDecoration: 'none'}} target="_blank" rel="noreferrer" aria-label="LinkedIn" href='https://www.linkedin.com/in/conner-pinson/'><FaLinkedin /></a>
         <a style={{color: 'white', textDecoration: 'none', paddingLeft: '10px'}} target="_blank" rel="noreferrer" aria-label="GitHub" href='https://github.com/pinsonc'><FaGithub /></a>
-        <a style={{color: 'white', textDecoration: 'none', paddingLeft: '10px'}} target="_blank" rel="noreferrer" aria-label="Twitter" href='https://twitter.com/pinsonneedles'><FaTwitter /></a>
+        <a style={{color: 'white', textDecoration: 'none', paddingLeft: '10px'}} target="_blank" rel="me noreferrer" aria-label="Mastodon" href="https://mstdn.social/@pinsoneedles"><FaMastodon/></a>
+        <a style={{color: 'white', textDecoration: 'none', paddingLeft: '10px'}} target="_blank" rel="noreferrer" aria-label="Keybase" href='https://keybase.io/pinsoneedles'><FaKeybase /></a>
         <a style={{color: 'white', textDecoration: 'none', paddingLeft: '10px'}} target="_blank" rel="noreferrer" aria-label="YouTube" href='https://www.youtube.com/channel/UC8oDiWMhnO3FqLTpYTpSOxA'><FaYoutube /></a>
         <a style={{color: 'white', textDecoration: 'none', paddingLeft: '10px'}} target="_blank" rel="noreferrer" aria-label="E-mail" href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;%63%6F%6E%6E%65%72%2E%70%69%6E%73%6F%6E%40%68%65%79%2E%63%6F%6D"><FaEnvelope /></a>
       </div>
